f9c2d87e64eeab1c15d31a32aa1e4ac274cc355e
Vitaly Khabarov <vitkhab@gmail.com>
2020-03-09 10:20:31 +0300

    Add helper functions

--- helpers.rb
+++ helpers.rb
@@ -1,3 +1,73 @@
+def flash_danger(message)
+  session[:flashes] << { type: 'alert-danger', message: message }
+end
+
+def flash_success(message)
+  session[:flashes] << { type: 'alert-success', message: message }
+end
+
+def log_event(type, name, message, params = '{}')
+  case type
+  when 'error'
+    logger.error('service=ui | ' \
+                 "event=#{name} | " \
+                 "request_id=#{request.env['REQUEST_ID']} | " \
+                 "message=\'#{message}\' | " \
+                 "params: #{params.to_json}")
+  when 'info'
+    logger.info('service=ui | ' \
+                "event=#{name} | " \
+                "request_id=#{request.env['REQUEST_ID']} | " \
+                "message=\'#{message}\' | " \
+                "params: #{params.to_json}")
+  when 'warning'
+    logger.warn('service=ui | ' \
+                "event=#{name} | " \
+                "request_id=#{request.env['REQUEST_ID']} | " \
+                "message=\'#{message}\' |  " \
+                "params: #{params.to_json}")
+  end
+end
+
+def http_healthcheck_handler()
+  status = 1
+
+  healthcheck = { status: status}
+  healthcheck.to_json
+end
+
+def healthcheck_handler(db_url)
+  begin
+    commentdb_test = Mongo::Client.new(db_url,
+                                       server_selection_timeout: 2)
+    commentdb_test.database_names
+    commentdb_test.close
+  rescue StandardError
+    commentdb_status = 0
+  else
+    commentdb_status = 1
+  end
+
+  status = commentdb_status
+  healthcheck = {
+    status: status,
+    dependent_services: {
+      commentdb: commentdb_status
+    }
+  }
+
+  healthcheck.to_json
+end
+
+def set_health_gauge(metric, value)
+  metric.set(
+    {
+      version: 1.0
+    },
+    value
+  )
+end
+
 helpers do
   # a helper method to turn a string ID
   # representation into a BSON::ObjectId
